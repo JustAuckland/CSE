5684750116c89c99ff84cf00615ce2aa358d4143
Auckland <r4rf@wetigers.org>
2017-11-28 15:08:10 -0800

    Adding Previous Notes
    11/27/17

---
+++ Notes.py
@@ -0,0 +1,17 @@
+print("Hello World")
+#Comment
+print(3+5)
+print(3**3)
+print()
+cat_name = "Kitty"
+cat_type = "White"
+cat_cylinders = 1
+cat_mpg = 12
+
+print("My cat is named %s. It is a %s cat" % (cat_name, cat_type))
+name = input ("what is your name?")
+print("Hello %s." % name)
+# print(name)
+
+age = input ("How old are you?")
+print("%s? Is that old enough to be watching that movie?" % age)
