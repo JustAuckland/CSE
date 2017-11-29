69714ce13afe7fbfb49fa8c7717474cd35fcec8a
Auckland <r4rf@wetigers.org>
2017-11-29 15:41:56 -0800

    11/29/17

--- Notes.py
+++ Notes.py
@@ -1,40 +1,77 @@
-print("Hello World")
-#Comment
-print(3+5)
-print(3**3)
-print()
-# cat_name = "Kitty"
-# cat_type = "White"
-# cat_cylinders = 1
-# cat_mpg = 12
+# print("Hello World")
+# #Comment
+# print(3+5)
+# print(3**3)
+# print()
+# # cat_name = "Kitty"
+# # cat_type = "White"
+# # cat_cylinders = 1
+# # cat_mpg = 12
+# #
+# # print("My cat is named %s. It is a %s cat" % (cat_name, cat_type))
+# # name = input ("what is your name?")
+# # print("Hello %s." % name)
+# # print(name)
 #
-# print("My cat is named %s. It is a %s cat" % (cat_name, cat_type))
-# name = input ("what is your name?")
-# print("Hello %s." % name)
-# print(name)
+# # age = input ("How old are you?")
+# # print("%s? Is that old enough to be watching that movie?" % age)
+#
+# def print_hw():
+#     print("Hello World")
+#
+#
+# print_hw()
+#
+#
+# def say_hi(name):
+#     print("Hello %s." % name)
+#     print("I hope you have a fantastic day.")
+#
+#
+# say_hi("Box Box")
+#
+#
+# def birthday(age):
+#     age+=1  # age=age+1
+#     print(age)
+#
+# say_hi("Box Box")
+# print("John is 15. Next year:")
+# birthday(15)
 
-# age = input ("How old are you?")
-# print("%s? Is that old enough to be watching that movie?" % age)
 
-def print_hw():
-    print("Hello World")
+def f(x):
+    return x**5 + 4 * x **4 - 17*x**2 + 4
 
 
-print_hw()
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage >= 80:
+        return "B"
+    elif percentage >= 70:
+        return "C"
+    elif percentage >= 60:
+        return "D"
+    else:
+        return "F"
 
 
-def say_hi(name):
-    print("Hello %s." % name)
-    print("I hope you have a fantastic day.")
+# for num in range(5):
+#     print(num+1)
 
 
-say_hi("Box Box")
+# for Cat in "Hello World":
+#     print(Cat)
 
 
-def birthday(age):
-    age+=1  # age=age+1
-    print(age)
+# response = ""
+# while response != "Hello":
+#     response = input("Say \"Hello\"")
 
-say_hi("Box Box")
-print("John is 15. Next year:")
-birthday(15)
+
+print ("Hello \nWorld")
+
+
+import random
+print(random.randint(0, 6))
