b29f2cfeeee9d90828778bb30019be7091b69664
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2018-01-22 20:24:36 -0800

    1/22/18

--- HangmanMeme - Auckland Van Saun.py
+++ HangmanMeme - Auckland Van Saun.py
@@ -11,15 +11,20 @@ Outline of Hangman
 
 """""
 
-bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu","Anivia", "Annie", "Ashe", "Azir", "Blitzcrank", "Brand", "Braum",
-"Caitlyn", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana", "DrMundo", "Draven", "Elise", "Evelynn", "Ezreal",
-"Fiddlesticks", "Fiora", "Fizz", "Galio", "Gangplank", "Garen", "Gnar", "Gragas", "Hecarim", "Heimerdinger", "Irelia",
-"Janna", "JarvanIV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus",
-KassadinKatarinaKayleKayneKennenKhaZixKogMawLeBlancLeeSinLeonaLissandraLucianLuluLuxMalphiteMalzaharMaokaiMasterYi
-Miss FortuneMordekaiserMorganaNamiNasusNautilusNidaleeNocturneNunuOlafOriannaOrnPantheonPoppyQuinnRammusRekSaiRenekton
-RengarRivenRumbleRyzeSejuaniShacoShenShyvanaSingedSionSivirSkarnerSonaSorakaSwainSyndraTalonTaricTeemoThreshTristana
-YasuoYorickZacZedZiggsZileanZoeZyra
-word = random.randint(0,134)
+bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "AuelionSol" "Azir", "Bard",
+        "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana",
+        "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
+        "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern",
+        "Janna", "JarvanIV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle",
+        "Kayne", "Kennen", "KhaZix", "Kindred", "Kled", "KogMaw", "LeBlanc", "LeeSin", "Leona", "Lissandra", "Lucian",
+        "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami",
+        "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Quinn",
+        "Rakan", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen",
+        "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah",
+        "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate","Twitch", "Udyr",
+        "Urgot", "Varus", "Vayne", "Veigar", "VelKoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong",
+        "Xayah", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
+word = random.randint(0, 134)
 print(bank[word])
 
 
