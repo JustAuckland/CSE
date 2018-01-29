027bd42570f33458639014aab7f84cc67c00c66e
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2018-01-29 15:42:55 -0800

    1/29/18

--- HangmanMeme - Auckland Van Saun.py
+++ HangmanMeme - Auckland Van Saun.py
@@ -1,9 +1,10 @@
 import random
+import string
 
-guessesLeft = 0
-lettersGuessed = []
+guessesLeft = 10
+lettersGuessed = list(string.punctuation + " ")
 
-bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "AurelionSol", "Azir", "Bard",
+bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Aurelion Sol", "Azir", "Bard",
         "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana",
         "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
         "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern",
@@ -13,46 +14,40 @@ bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe"
         "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Quinn",
         "Rakan", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen",
         "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah",
-        "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "TwistedFate", "Twitch", "Udyr",
+        "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr",
         "Urgot", "Varus", "Vayne", "Veigar", "VelKoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong",
         "Xayah", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
 
 word = random.choice(bank)
-word = word.lower()
 
 wordList = list(word)
 guess = []
+response = input("Type anything to begin : ")
 
-
-response = input("Type anyhting to begin : ")
-
-while guessesLeft != 10:
+while guessesLeft != 0:
         output = []
 
         for letter in word:
-                if letter in lettersGuessed:
-                        output.append(letter)
 
+                if letter.lower() in lettersGuessed:
+                        output.append(letter)
                 else:
-                        output.append("_ ")
+                        output.append("*")
         if output == wordList:
                 print(word)
                 print("Good job you're an exper!")
                 exit(0)
 
-        oKKo = "".join(output)
-        print(oKKo)
-        guessesLeft += 1
+        join = "".join(output)
+        print(join)
+        guess = input("Make a guess : ").lower()
+        if guess == word:
+                guessesLeft -= 1
 
-        guess = input("Make a guess : ")
         if len(guess) != 1:
                 print("Please guess one letter at a time!")
         elif guess not in 'abcdefghijklmnopqrstuvwxyz':
                 print("Please only guess letters!")
-        for letter in output:
-                if guess == letter in output:
-                        print("You've already guessed this letter")
-                        guessesLeft -= 1
 
         lettersGuessed.append(guess)
 
