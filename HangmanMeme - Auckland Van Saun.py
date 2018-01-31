1bf0036bd30581571fea35b5235d8c80d87692da
JustAuckland <31050482+JustAuckland@users.noreply.github.com>
2018-01-30 19:44:53 -0800

    1/30/18

--- HangmanMeme - Auckland Van Saun.py
+++ HangmanMeme - Auckland Van Saun.py
@@ -1,47 +1,54 @@
+
 import random
 import string
+
+# def script():
 
 guessesLeft = 10
 lettersGuessed = list(string.punctuation + " ")
 
 bank = ["Aatrox", "Ahri", "Akali", "Alistar", "Amumu", "Anivia", "Annie", "Ashe", "Aurelion Sol", "Azir", "Bard",
-        "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "ChoGath", "Corki", "Darius", "Diana",
-        "DrMundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
+        "Blitzcrank", "Brand", "Braum", "Caitlyn", "Camille", "Cassiopeia", "Cho'Gath", "Corki", "Darius", "Diana",
+        "Dr.Mundo", "Draven", "Ekko", "Elise", "Evelynn", "Ezreal", "Fiddlesticks", "Fiora", "Fizz", "Galio",
         "Gangplank", "Garen", "Gnar", "Gragas", "Graves", "Hecarim", "Heimerdinger", "Illaoi", "Irelia", "Ivern",
-        "Janna", "JarvanIV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle",
-        "Kayn", "Kennen", "KhaZix", "Kindred", "Kled", "KogMaw", "LeBlanc", "LeeSin", "Leona", "Lissandra", "Lucian",
-        "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "MasterYi", "MissFortune", "Mordekaiser", "Morgana", "Nami",
+        "Janna", "Jarvan IV", "Jax", "Jayce", "Jinx", "Kalista", "Karma", "Karthus", "Kassadin", "Katarina", "Kayle",
+        "Kayn", "Kennen", "Kha'Zix", "Kindred", "Kled", "Kog'Maw", "LeBlanc", "Lee Sin", "Leona", "Lissandra", "Lucian",
+        "Lulu", "Lux", "Malphite", "Malzahar", "Maokai", "Master Yi", "Miss Fortune", "Mordekaiser", "Morgana", "Nami",
         "Nasus", "Nautilus", "Nidalee", "Nocturne", "Nunu", "Olaf", "Orianna", "Ornn", "Pantheon", "Poppy", "Quinn",
-        "Rakan", "Rammus", "RekSai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen",
-        "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "TahmKench", "Taliyah",
+        "Rakan", "Rammus", "Rek'Sai", "Renekton", "Rengar", "Riven", "Rumble", "Ryze", "Sejuani", "Shaco", "Shen",
+        "Shyvana", "Singed", "Sion", "Sivir", "Skarner", "Sona", "Soraka", "Swain", "Syndra", "Tahm Kench", "Taliyah",
         "Talon", "Taric", "Teemo", "Thresh", "Tristana", "Trundle", "Tryndamere", "Twisted Fate", "Twitch", "Udyr",
-        "Urgot", "Varus", "Vayne", "Veigar", "VelKoz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong",
-        "Xayah", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
+        "Urgot", "Varus", "Vayne", "Veigar", "Vel'Koz", "Vi", "Viktor", "Vladimir", "Volibear", "Warwick", "Wukong",
+        "Xayah", "Xin Zhao", "Yasuo", "Yorick", "Zac", "Zed", "Ziggs", "Zilean", "Zoe", "Zyra"]
 
 word = random.choice(bank)
 
 wordList = list(word)
 guess = []
-response = input("Type anything to begin : ")
+guess1 = ()
+# response = input("Type anything to begin : ")
 
-while guessesLeft != 0:
+
+while guessesLeft > 0:
         output = []
 
         for letter in word:
-
                 if letter.lower() in lettersGuessed:
                         output.append(letter)
                 else:
-                        output.append("*")
+                        output.append("_")
+
         if output == wordList:
                 print(word)
                 print("Good job you're an exper!")
                 exit(0)
 
-        join = "".join(output)
+        join = " ".join(output)
         print(join)
+        print(guessesLeft)
         guess = input("Make a guess : ").lower()
-        if guess == word:
+
+        if guess1 != word:
                 guessesLeft -= 1
 
         if len(guess) != 1:
@@ -49,6 +56,18 @@ while guessesLeft != 0:
         elif guess not in 'abcdefghijklmnopqrstuvwxyz':
                 print("Please only guess letters!")
 
-        lettersGuessed.append(guess)
+        lettersGuessed.append(guess) and guess1
 
-print("Ripperoni Pepperoni \nBetter luck next time!")
+# if output != wordList:
+#         print("Ripperoni Pepperoni \nBetter luck next time!")
+# else:
+#         end = input("Your word was %s\nPress anything to restart" % word)
+#         if end.lower() == "no" or "n":
+#                 exit(0)
+#         elif end.lower() != "yes" or "y":
+#                 "please guess \"Yes\" or \"No\""
+#         else:
+#                 test = 0
+print("Ripperoni Pepperoni\n your word was %s" % word)
+
+# script()
